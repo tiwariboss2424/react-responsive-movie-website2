1677f5306b718ecf74b043e34deb800258f44eaa
Niranjan Tiwari <120399001+tiwariboss2424@users.noreply.github.com>
2023-05-12 14:05:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./page/home/Home";
+import MovieList from "./page/movielist/MovieList";
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route>
+            <Route index element={<Home />}></Route>
+            <Route
+              path="movie/:id"
+              element={<h1>Movie Detail Page</h1>}
+            ></Route>
+            <Route path="movies/:type" element={<MovieList />}></Route>
+            <Route path="/*" element={<h1>Error Page</h1>}></Route>
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
